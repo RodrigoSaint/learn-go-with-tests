0755675336fc89fe4d4891aafb84e3311671bb43
Rodrigo Santos da Silva <rodrigosantosdasilva@Rodrigos-MacBook-Pro.local>
2025-01-02 19:57:21 +0100

    Adding example description

--- lesson-02/adder_test.go
+++ lesson-02/adder_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// Example function used on documentations. You can run its tests by executing go test -v
+// The comment with the output is validated, if you change to something else it will break the example
 func ExampleAdd() {
 	sum := Add(2, 2)
 	fmt.Println(sum)
