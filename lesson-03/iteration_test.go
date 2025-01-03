2b361a95b5000583983b2a72189f421af65255bf
Rodrigo Santos da Silva <rodrigosantosdasilva@Rodrigos-MacBook-Pro.local>
2025-01-03 17:54:49 +0100

    Adding example function

--- lesson-03/iteration_test.go
+++ lesson-03/iteration_test.go
@@ -1,11 +1,20 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 4)
 	}
+}
+
+func ExampleRepeat() {
+	text := Repeat("a", 4)
+	fmt.Print(text)
+	// Output: aaaa
 }
 
 func TestRepeating(t *testing.T) {
