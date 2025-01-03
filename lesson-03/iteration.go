523c9f6bcdfebab59b87ab07f0da88a57f2648d0
Rodrigo Santos da Silva <rodrigosantosdasilva@Rodrigos-MacBook-Pro.local>
2025-01-03 17:59:33 +0100

    Adding string builder

--- lesson-03/iteration.go
+++ lesson-03/iteration.go
@@ -1,9 +1,15 @@
 package iteration
 
+import "strings"
+
 func Repeat(word string, limit int) string {
 	repeated := ""
 	for i := 0; i < limit; i++ {
 		repeated += word
 	}
 	return repeated
+}
+
+func RepeatBuilder(word string, limit int) string {
+	return strings.Repeat(word, limit)
 }
--- lesson-03/iteration_test.go
+++ lesson-03/iteration_test.go
@@ -5,14 +5,27 @@ import (
 	"testing"
 )
 
+// Benchmark tests can be run with `go test --bench=.`
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 4)
 	}
 }
 
+func BenchmarkRepeatBuilder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RepeatBuilder("a", 4)
+	}
+}
+
 func ExampleRepeat() {
 	text := Repeat("a", 4)
+	fmt.Print(text)
+	// Output: aaaa
+}
+
+func ExampleRepeatBuilder() {
+	text := RepeatBuilder("a", 4)
 	fmt.Print(text)
 	// Output: aaaa
 }
